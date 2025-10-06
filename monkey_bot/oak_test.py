4ac651cfa6dd62ece7f49ec5cccbff118063df36
HTr04 <huytranlam123@gmail.com>
2025-10-05 23:05:08 -0700

    fix

--- monkey_bot/oak_test.py
+++ monkey_bot/oak_test.py
@@ -1,16 +1,21 @@
 import depthai as dai
 import cv2
 
+# Build pipeline
 pipeline = dai.Pipeline()
-cam = pipeline.createColorCamera()
+
+cam = pipeline.create(dai.node.ColorCamera)
 cam.setPreviewSize(640, 480)
 cam.setInterleaved(False)
-xout = pipeline.createXLinkOut()
+cam.setFps(30)
+
+xout = pipeline.create(dai.node.XLinkOut)
 xout.setStreamName("rgb")
 cam.preview.link(xout.input)
 
-with dai.Device(pipeline) as device:
-    q = device.getOutputQueue("rgb", maxSize=4, blocking=False)
+# Run
+with dai.Device(pipeline) as dev:
+    q = dev.getOutputQueue("rgb", maxSize=4, blocking=False)
     while True:
         frame = q.get().getCvFrame()
         cv2.imshow("OAK-D Preview", frame)
